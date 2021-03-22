76ddbd6bb77628da6eb2ca64ec7f198ff3ba35a1
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-03-22 23:28:36 +0200

    Created a template for image card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import './styles.css';
+import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
+import '../node_modules/@pnotify/core/dist/PNotify.css';
+import '../node_modules/@pnotify/mobile/dist/PNotifyMobile.css';
